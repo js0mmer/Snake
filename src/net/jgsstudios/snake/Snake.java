aedb2566b6881813598a94d585fd05f08afbce4e
Jacob Sommer <js0mmer@users.noreply.github.com>
2016-11-16 21:30:08 -0800

    Update Snake.java

--- src/net/jgsstudios/snake/Snake.java
+++ src/net/jgsstudios/snake/Snake.java
@@ -1,75 +1,86 @@
-package net.jgsstudios.snake;
+package tk.jsommer.snake;
 
 import java.awt.Point;
 
 public class Snake {
-	
+
 	public Point[] tail = new Point[100];
-	
+
 	public boolean dead;
 	private boolean grow;
-	
+
 	/**
 	 * 0=Right, 1=Down, 2=Left, 3=Up
 	 */
-	
+
 	public int moveDirection;
 	public int tailSize;
 	public int length;
-	
+
 	public int maxMoveSpeed;
 	private int moveSpeed;
-	
+
 	private Food[] food;
-	
-	public Snake(Food[] food){
+
+	public Snake(Food[] food) {
 		this.tail[0] = new Point(10, 10);
 		this.length = 1;
-		
+
 		this.food = food;
 		this.maxMoveSpeed = 10;
-		
+
 		this.tailSize = 40;
-}
-	
-	public void update(){
-		if(moveSpeed >= maxMoveSpeed){
-		if(moveDirection == 0)updateSnakeTail(1, 0);
-		if(moveDirection == 1)updateSnakeTail(0, 1);
-		if(moveDirection == 2)updateSnakeTail(-1, 0);
-		if(moveDirection == 3)updateSnakeTail(0, -1);
-		moveSpeed = 0;
-		
-		checkDeath();
-		eatFood();
-		}else{
+	}
+
+	public void update() {
+		if(moveSpeed >= maxMoveSpeed) {
+			switch(moveDirection) {
+				case(0):
+					updateSnakeTail(1, 0);
+					break;
+				case(1):
+					updateSnakeTail(0, 1);
+					break;
+				case(2):
+					updateSnakeTail(-1, 0);
+					break;
+				case(3):
+					updateSnakeTail(0, -1);
+					break;
+			}
+
+			moveSpeed = 0;
+
+			checkDeath();
+			eatFood();
+		} else {
 			moveSpeed++;
 		}
 	}
-	
+
 	private void eatFood(){
-		for(int i = 0; i < food.length; i++){
+		for(int i = 0; i < food.length; i++) {
 			if(food[i] != null){
-				if(tail[0].x == food[i].x && tail[0].y == food[i].y){
+				if(tail[0].x == food[i].x && tail[0].y == food[i].y) {
 					food[i] = null;
-					grow=true;
+					grow = true;
 					break;
 				}
 			}
 		}
 	}
-	
-	private void checkDeath(){
-		if(tail[0].x < 0||tail[0].x > Frame.WIDTH/tailSize-1) dead=true;
-		if(tail[0].y < 0||tail[0].y > Frame.HEIGHT/tailSize-1) dead=true;
-		
-		for(int i = 0; i < tail.length; i++){
-			if(tail[i] != null){
-			for(int j = 0; j < tail.length; j++){
-				if(i != j){
-					if(tail[j] != null){
-					if(tail[i].x == tail[j].x && tail[i].y == tail[j].y){
-						dead = true;
+
+	private void checkDeath() {
+		if(tail[0].x < 0 || tail[0].x > Frame.WIDTH/tailSize - 1) dead = true;
+		if(tail[0].y < 0 || tail[0].y > Frame.HEIGHT/tailSize - 1) dead = true;
+
+		for(int i = 0; i < tail.length; i++) {
+			if(tail[i] != null) {
+				for(int j = 0; j < tail.length; j++){
+					if(i != j) {
+						if(tail[j] != null) {
+							if(tail[i].x == tail[j].x && tail[i].y == tail[j].y) {
+								dead = true;
 							}
 						}
 					}
@@ -77,31 +88,27 @@ public class Snake {
 			}
 		}
 	}
-	
-	private void updateSnakeTail(int x, int y){
+
+	private void updateSnakeTail(int x, int y) {
 		Point lastTail = (Point) tail[0].clone();
-		
-		tail[0].x+=x;
-		tail[0].y+=y;
-		for(int i = 1; i < tail.length; i++){
-			if(tail[i] != null){
+
+		tail[0].x += x;
+		tail[0].y += y;
+
+		for(int i = 1; i < tail.length; i++) {
+			if(tail[i] != null) {
 				Point point = (Point) tail[i].clone();
-				
-				
-				
-				tail[i]=lastTail;
-				
+
+				tail[i] = lastTail;
 				lastTail = point;
-				
-				
 			}
-			
-			if(grow && i == length){
-				tail[i+1]=(Point) lastTail.clone();
+
+			if(grow && i == length) {
+				tail[i + 1] = (Point) lastTail.clone();
 				length++;
-				grow=false;
+				grow = false;
+			}
 		}
+
 	}
-	
-}
 }
